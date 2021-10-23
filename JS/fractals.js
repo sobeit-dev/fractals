4ae6d3462fbf4b2274d1a1b2d2cfdd4dae240a28
sobeit-dev <github_dev@mail.ru>
2021-10-23 04:24:33 +0300

    mandelbrot_and_fix

--- JS/fractals.js
+++ JS/fractals.js
@@ -10,8 +10,7 @@ let B_BUTTON = document.getElementById("build");
 
 // C_SETTINGS
 let fract_list = ListBoxInit("Тип:",C_SETTINGS);
-let p_width_i = AddIntInput(1, 5, 2, "Толщина линии:", C_SETTINGS);
-let p_color_i = AddColorInput("#e66465", "Цвет линии:", C_SETTINGS);
+let p_color_i = AddColorInput("#e66465", "Цвет:", C_SETTINGS);
 // E_SETTINGS
 RenderESettings(E_SETTINGS);
 // ---------- MENU FUNCTION ----------
@@ -66,7 +65,7 @@ function ListBoxInit(text, parent)
 
     //
         AddInListBox(select, "Дерево",  0);
-        AddInListBox(select, "NULL TEST",  1);
+        AddInListBox(select, "Мандельброт",  1);
     //
 
     div.append(p);
@@ -90,8 +89,18 @@ function RenderESettings(parent)
     {
         case 0: // tree
         {
+            EInputs.p_width_i = AddIntInput(1, 5, 2, "Толщина линии:", parent);
             EInputs.level = AddIntInput(1, 16, 10, "Глубина:", parent);
             EInputs.angle = AddIntInput(1, 180, 15, "Угол:", parent);
+            break;
+        }
+        case 1: // mandelbrot
+        {
+            EInputs.level = AddIntInput(5, 3000, 100, "Глубина:", parent);
+            EInputs.size = AddIntInput(200, 6000, 2000, "Разрешение:", parent);
+            // EInputs.bX = AddIntInput(-3000, 3000, 0, "X:", parent);
+            // EInputs.bY = AddIntInput(-3000, 3000, 0, "Y:", parent);
+            // EInputs.bZ = AddIntInput(-3000, 3, 2, "Масштаб:", parent);
             break;
         }
         default:
@@ -112,7 +121,32 @@ B_BUTTON.onclick = function()
     {
         case 0: // tree
         {
-            DrawTree(0, 0, -90, Number(EInputs.level.value) | 2);
+            RENDER_WIN_SVG.style.display = "";
+            RENDER_WIN_CANVAS.style.display = "none";
+            DrawTree(0, 0, -90, Number(EInputs.level.value));
+            break;
+        }
+        case 1: // mandelbrot
+        {
+            RENDER_WIN_SVG.style.display = "none";
+            RENDER_WIN_CANVAS.style.display = "";
+            RENDER_WIN_CANVAS.width = EInputs.size.value;
+            RENDER_WIN_CANVAS.height = EInputs.size.value;
+
+            // mandelbrot( RENDER_WIN_CANVAS,
+            //             EInputs.bY.value-EInputs.bZ.value,
+            //             EInputs.bX.value+EInputs.bZ.value,
+            //             EInputs.bX.value-EInputs.bZ.value,
+            //             EInputs.bY.value+EInputs.bZ.value,
+            //             EInputs.level.value, p_color_i.value);
+
+            mandelbrot( RENDER_WIN_CANVAS,
+                        -2,
+                        2,
+                        -2,
+                        2,
+                        EInputs.level.value, p_color_i.value);
+
             break;
         }
         default:
@@ -148,7 +182,17 @@ for(let input in EInputs)
 function DrawLine(x1, y1, x2, y2)
 {
     let line = RENDER_WIN.line(x1, y1, x2, y2);
-    line.stroke({ color: p_color_i.value, width: p_width_i.value, linecap: 'round' });
+    line.stroke({ color: p_color_i.value, width: EInputs.p_width_i.value, linecap: 'round' });
+}
+
+function hexToRgb(hex) 
+{
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
 }
 
 // ---------- FRACTALS FUNCTION ----------
@@ -166,4 +210,72 @@ function DrawTree(x1, y1, angle, level)
         DrawTree(x2, y2, angle + Number(EInputs.angle.value), level - 1);
     }
 }
-DrawTree(0, 0, -90, Number(EInputs.level.value)); SetViewBox();
+
+window.onload = function() 
+{
+    //костыльная инициализация первого фрактала
+    DrawTree(0, 0, -90, Number(EInputs.level.value));
+    SetViewBox();
+    RENDER_WIN.clear();
+    DrawTree(0, 0, -90, Number(EInputs.level.value));
+};
+
+// mandelbrot
+function mandelbrotIteration (cx, cy, maxIter) {
+    var x = 0.0;
+    var y = 0.0;
+    var xx = 0;
+    var yy = 0;
+    var xy = 0;
+    var i = maxIter;
+    while (i-- && xx + yy <= 4) {
+     xy = x * y;
+     xx = x * x;
+     yy = y * y;
+     x = xx - yy + cx;
+     y = xy + xy + cy;
+    }
+    return maxIter - i;
+   }
+    
+   function mandelbrot (canvas, xmin, xmax, ymin, ymax, iterations, color_s) {
+    var width = canvas.width;
+    var height = canvas.height;
+    var context = canvas.getContext('2d');
+    var image = context.getImageData(0, 0, width, height);
+    var pixels = image.data;
+   
+    for (var ix = 0; ix < width; ++ix) {
+     for (var iy = 0; iy < height; ++iy) {
+      var x = xmin + (xmax - xmin) * ix / (width - 1);
+      var y = ymin + (ymax - ymin) * iy / (height - 1);
+      var i = mandelbrotIteration(x, y, iterations);
+      var pixels_position = 4 * (width * iy + ix);
+      if (i > iterations) {
+       pixels[pixels_position] = 0;
+       pixels[pixels_position + 1] = 0;
+       pixels[pixels_position + 2] = 0;
+      } 
+      else {
+       var color = 2 * Math.log(i) / Math.log(iterations - 1.0);
+       if (color < 1) {
+        pixels[pixels_position] = hexToRgb(color_s).r * color;
+        pixels[pixels_position + 1] = hexToRgb(color_s).g;
+        pixels[pixels_position + 2] = hexToRgb(color_s).b;
+       }
+       else if ( color < 2 ) {
+        pixels[pixels_position] = hexToRgb(color_s).r;
+        pixels[pixels_position + 1] = hexToRgb(color_s).g * (color - 1);
+        pixels[pixels_position + 2] = hexToRgb(color_s).b;
+       } 
+       else {
+        pixels[pixels_position] = hexToRgb(color_s).r;
+        pixels[pixels_position + 1] = hexToRgb(color_s).g;
+        pixels[pixels_position + 2] = hexToRgb(color_s).b * (color - 2);
+       }
+      }
+      pixels[pixels_position + 3] = 255;
+     }
+    }
+    context.putImageData(image, 0, 0);
+   }
--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,16 @@
 RENDER_WIN = SVG().addTo('#main-render-win');
-let RENDER_BOX = document.getElementById("main-render-win");
-let RENDER_WIN_ELEMENT = RENDER_BOX.childNodes[0];
+RENDER_BOX = document.getElementById("main-render-win");
+RENDER_WIN_CANVAS = document.createElement('canvas');
+RENDER_WIN_CANVAS.style.display = "none";
+RENDER_WIN_SVG = RENDER_BOX.childNodes[0];
+RENDER_BOX.append(RENDER_WIN_CANVAS);
 
 // ---------- ZOOM AND MOVE ----------
 
 function SetViewBox() 
 {
-    const box = RENDER_WIN_ELEMENT.getBBox();
-    RENDER_WIN_ELEMENT.setAttribute('viewBox', `${box.x - 10} ${box.y - 10} ${box.width + 20} ${box.height + 20}`);
+    const box = RENDER_WIN_SVG.getBBox();
+    RENDER_WIN_SVG.setAttribute('viewBox', `${box.x - 10} ${box.y - 10} ${box.width + 20} ${box.height + 20}`);
 }
 
 let 
@@ -15,23 +18,26 @@ let
     pointX = 0,
     pointY = 0,
     scale = 1,
-    start = { x: 0, y: 0 };
+    start = { x: 0, y: 0 },
+    is_double_t = false;
+    t_dist = 0;
 
 function setTransform() 
 {
-    RENDER_WIN_ELEMENT.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+    RENDER_WIN_SVG.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+    RENDER_WIN_CANVAS.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
 }
 
-RENDER_WIN_ELEMENT.onmousedown = function (e) 
+RENDER_BOX.onmousedown = function (e) 
 {
     e.preventDefault();
     start = { x: e.clientX - pointX, y: e.clientY - pointY };
     panning = true;
 }
 
-RENDER_WIN_ELEMENT.onmouseup = function (e) {panning = false;}
+RENDER_BOX.onmouseup = function (e) {panning = false;}
 
-RENDER_WIN_ELEMENT.onmousemove = function (e) 
+RENDER_BOX.onmousemove = function (e) 
 {
     e.preventDefault();
     if (!panning) return;
@@ -41,7 +47,7 @@ RENDER_WIN_ELEMENT.onmousemove = function (e)
     setTransform();
 }
 
-RENDER_WIN_ELEMENT.onwheel = function (e) 
+RENDER_BOX.onwheel = function (e) 
 {
     e.preventDefault();
     let 
@@ -60,4 +66,90 @@ RENDER_WIN_ELEMENT.onwheel = function (e)
     pointX = e.clientX - xs * scale;
     pointY = e.clientY - ys * scale;
     setTransform();
+}
+
+SenseInit();
+
+function SenseInit() 
+{
+    RENDER_BOX.addEventListener("touchstart", SenseStart, false);
+    RENDER_BOX.addEventListener("touchend", SenseEnd, false);
+    RENDER_BOX.addEventListener("touchmove", SenseMove, false);
+}
+
+function SenseStart(evt) 
+{
+    evt.preventDefault();
+    let touches = evt.changedTouches;
+    switch(evt.touches.length) 
+    {
+        case 1:
+        {
+            start = { x: touches[0].pageX - pointX, y: touches[0].pageY - pointY };
+            panning = true;
+            break;
+        }
+        case 2:
+        {
+            is_double_t = true;
+            break;
+        } 
+        default: break;
+    }
+}
+
+function SenseEnd(evt)
+{
+    evt.preventDefault();
+    switch (evt.touches.length) 
+    {
+        case 1:
+        {
+            panning = false;
+            break;
+        }
+        default: break;
+    }
+}
+
+function SenseMove(evt)
+{
+    evt.preventDefault();
+    let touches = evt.changedTouches;
+    switch (evt.touches.length) 
+    {
+        case 1:
+        {
+            if (!panning) return;
+            pointX = (touches[0].pageX - start.x);
+            pointY = (touches[0].pageY - start.y);
+            setTransform();
+            break;
+        }
+        case 2:
+        {
+            if(is_double_t)
+            {
+                t_dist = Math.sqrt(Math.pow((touches[1].pageX - touches[0].pageX), 2) + Math.pow((touches[1].pageY - touches[0].pageY), 2));
+                is_double_t = false;
+            }
+            let l_t_dist = Math.sqrt(Math.pow((touches[1].pageX - touches[0].pageX), 2) + Math.pow((touches[1].pageY - touches[0].pageY), 2));
+
+            let 
+                xs = ((touches[1].pageX + touches[0].pageX) / 2 - pointX) / scale,
+                ys = ((touches[1].pageY + touches[0].pageY) / 2 - pointY) / scale;
+
+            if(t_dist < l_t_dist) scale *= 1.03;
+            if(t_dist > l_t_dist)
+                if(scale < 0.5) return;
+                else scale /= 1.03;
+
+            pointX = ((touches[1].pageX + touches[0].pageX) / 2) - xs * scale;
+            pointY = ((touches[1].pageY + touches[0].pageY) / 2) - ys * scale;
+
+            setTransform();
+            break;
+        }
+        default: break;
+    }
 }
